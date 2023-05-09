bd6707c5d3e42806293305955454fc1742cba810
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-09 20:45:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 function sortTable() {
-	var table = document.querySelector('table');
-	var tbody = table.querySelector('tbody');
-	var rows = Array.from(tbody.querySelectorAll('tr'));
-	rows.sort(function(a, b) {
-		var aPoints = parseInt(a.cells[1].textContent);
-		var bPoints = parseInt(b.cells[1].textContent);
-		return bPoints - aPoints;
-	});
-	tbody.innerHTML = "";
-	rows.forEach(function(row) {
-		tbody.appendChild(row);
-	});
+  var table = document.querySelector('table');
+  var tbody = table.querySelector('tbody');
+  var rows = Array.from(tbody.querySelectorAll('tr'));
+  rows.sort(function(a, b) {
+    var aPoints = parseInt(a.cells[1].textContent);
+    var bPoints = parseInt(b.cells[1].textContent);
+    return bPoints - aPoints;
+  });
+  tbody.innerHTML = "";
+  rows.forEach(function(row) {
+    tbody.appendChild(row);
+  });
 }
 
 function addPoints(row) {
-	var pointsInput = row.querySelector('input[type="number"]');
-	var points = parseInt(pointsInput.value);
-	var currentPoints = parseInt(row.cells[1].textContent);
-	row.cells[1].textContent = currentPoints + points;
-	sortTable();
+  var pointsInput = row.querySelector('input[type="number"]');
+  var points = parseInt(pointsInput.value);
+  if (!isNaN(points)) {
+    var currentPoints = parseInt(row.cells[1].textContent);
+    row.cells[1].textContent = currentPoints + points;
+    pointsInput.value = "";
+    sortTable();
+  }
 }
-
-function subtractPoints(row) {
-	var pointsInput = row.querySelector('input[type="number"]');
-	var points = parseInt(pointsInput.value);
-	var currentPoints = parseInt(row.cells[
