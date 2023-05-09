54ae568f24c63d85e828e2b568d744c46ce47ce7
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-09 18:20:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,5 +14,17 @@ function sortTable() {
 
 function increasePoints(row) {
 	var pointsCell = row.cells[1];
-	var
+	var points = parseInt(pointsCell.textContent);
+	pointsCell.textContent = points + 1;
+	sortTable();
+}
+
+function decreasePoints(row) {
+	var pointsCell = row.cells[1];
+	var points = parseInt(pointsCell.textContent);
+	if (points > 0) {
+		pointsCell.textContent = points - 1;
+		sortTable();
+	}
+}
 
