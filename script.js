14ca09c8535304702421b835a30e2be0978d834c
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-09 18:46:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
-function sortTable() {
-	var table = document.querySelector('table');
-	var tbody = table.querySelector('tbody');
-	var rows = Array.from(tbody.querySelectorAll('tr'));
-	rows.sort(function(a, b) {
-		var aPoints = parseInt(a.cells[1].textContent);
-		var bPoints = parseInt(b.cells[1].textContent);
-		return bPoints - aPoints;
-	});
-	rows.forEach(function(row) {
-		tbody.appendChild(row);
-	});
-}
-
-function increasePoints(row) {
-	var pointsCell = row.cells[1];
-	var points = parseInt(pointsCell.textContent);
-	pointsCell.textContent = points + 1;
-}
-
-function decreasePoints(row) {
-	var pointsCell = row.cells[1];
-	var points = parseInt(pointsCell.textContent);
-	if (points > 0) {
-		pointsCell.textContent = points - 1;
-	}
-}
+const tableRows = document.querySelectorAll("tbody tr");
 
 function updateTable() {
-	sortTable();
+  [...tableRows]
+    .sort((rowA, rowB) => Number(rowB.cells[1].textContent) - Number(rowA.cells[1].textContent))
+    .forEach((row, index) => row.cells[0].textContent = `${index + 1}. ${row.cells[0].textContent}`);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	sortTable();
-});
+tableRows.forEach(row => {
+  const plusButton = row.querySelector(".plus");
+  const minusButton = row.querySelector(".minus");
+  const pointCell = row.querySelector("td:nth-child(2)");
 
-var updateButton = document.getElementById('update-button');
-updateButton.addEventListener('click', updateTable);
+  plusButton.addEventListener("click", () => {
+    const currentPoints = Number(pointCell.textContent);
+    pointCell.textContent = currentPoints + 1;
+    updateTable();
+  });
+
+  minusButton.addEventListener("click", () => {
+    const currentPoints = Number(pointCell.textContent);
+    if (currentPoints > 0) {
+      pointCell.textContent = currentPoints - 1;
+      updateTable();
+    }
+  });
+});
