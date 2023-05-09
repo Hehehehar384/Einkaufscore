c500d39182b162e2ffc2b393e48ea4de6261d019
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-09 19:25:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,25 +6,4 @@ function sortTable() {
 		var aPoints = parseInt(a.cells[1].textContent);
 		var bPoints = parseInt(b.cells[1].textContent);
 		return bPoints - aPoints;
-	});
-	rows.forEach(function(row) {
-		tbody.appendChild(row);
-	});
-}
-
-function increasePoints(row) {
-	var pointsCell = row.cells[1];
-	var points = parseInt(pointsCell.textContent);
-	pointsCell.textContent = points + 1;
-	sortTable();
-}
-
-function decreasePoints(row) {
-	var pointsCell = row.cells[1];
-	var points = parseInt(pointsCell.textContent);
-	if (points > 0) {
-		pointsCell.textContent = points - 1;
-		sortTable();
-	}
-}
 	
