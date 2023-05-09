d4d296640d48c5d3fa69f485cdeb16f75bf6445a
Hehehehar384 <132939788+Hehehehar384@users.noreply.github.com>
2023-05-09 18:39:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,15 +16,13 @@ function increasePoints(row) {
 	var pointsCell = row.cells[1];
 	var points = parseInt(pointsCell.textContent);
 	pointsCell.textContent = points + 1;
-	sortTable();
 }
 
 function decreasePoints(row) {
 	var pointsCell = row.cells[1];
-	var points = parseInt(row.cells[1].textContent);
+	var points = parseInt(pointsCell.textContent);
 	if (points > 0) {
-		row.cells[1].textContent = points - 1;
-		sortTable();
+		pointsCell.textContent = points - 1;
 	}
 }
 
@@ -34,21 +32,7 @@ function updateTable() {
 
 document.addEventListener('DOMContentLoaded', function() {
 	sortTable();
-	setInterval(sortTable, 5000); // Automatische Aktualisierung alle 5 Sekunden
 });
 
 var updateButton = document.getElementById('update-button');
 updateButton.addEventListener('click', updateTable);
-
-var rows = document.getElementsByTagName('tr');
-for (var i = 1; i < rows.length; i++) {
-	var row = rows[i];
-	var plusButton = row.querySelector('.plus');
-	var minusButton = row.querySelector('.minus');
-	plusButton.addEventListener('click', function() {
-		increasePoints(this.parentNode.parentNode);
-	});
-	minusButton.addEventListener('click', function() {
-		decreasePoints(this.parentNode.parentNode);
-	});
-}
